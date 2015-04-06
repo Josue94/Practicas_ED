c6f12f7959b59ec82f1a79479f6ac7d8668bb15e
Josué <i22feloj@i22feloj-K54C.(none)>
2015-04-06 18:04:00 +0200

    Terminada función insertar

--- practica2ed/principal.cpp
+++ practica2ed/principal.cpp
@@ -23,14 +23,31 @@ int main()
 	cout <<l.getCabeza()->getItem()<<endl;
 	l.insertar(9);
 	cout <<l.getCabeza()->getItem()<<endl;
+
+
 	cout<<l.getCabeza()->getSiguiente()->getItem()<<endl;
+	//cout<<l.getCabeza()->getSiguiente()->getAnterior()->getItem()<<endl;
+
+
 	l.insertar(12);
 	cout <<l.getCabeza()->getItem()<<endl;
 	cout<<l.getCabeza()->getSiguiente()->getItem()<<endl;
-	l.insertar(15);
+	cout<<l.getCabeza()->getSiguiente()->getSiguiente()->getItem()<<endl;
+
+	l.insertar(10);
 	cout <<l.getCabeza()->getItem()<<endl;
 	cout<<l.getCabeza()->getSiguiente()->getItem()<<endl;
+	cout<<l.getCabeza()->getSiguiente()->getSiguiente()->getAnterior()->getItem()<<endl;//tiene que dar 9
+	cout<<l.getCabeza()->getSiguiente()->getSiguiente()->getSiguiente()->getItem()<<endl;
+/*
+	l.insertar(20);
+	cout <<l.getCabeza()->getItem()<<endl;
+		cout<<l.getCabeza()->getSiguiente()->getItem()<<endl;
+		cout<<l.getCabeza()->getSiguiente()->getSiguiente()->getItem()<<endl;
+		cout<<l.getCabeza()->getSiguiente()->getSiguiente()->getSiguiente()->getItem()<<endl;
+		cout<<l.getCabeza()->getSiguiente()->getSiguiente()->getSiguiente()->getSiguiente()->getSiguiente()<<endl;
 
+*/
 	return 0;
 }
 
